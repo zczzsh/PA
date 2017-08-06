1c428e2c25b587eaf89597deda849bfb81728928
tracer-ics2015 <tracer@njuics.org>
2017-08-06 15:18:27 +0800

    >  compile NEMU 141220000 zhusihan Linux zhusihan-pc 4.9.34-1-MANJARO #1 SMP PREEMPT Sat Jun 24 08:38:10 UTC 2017 i686 GNU/Linux  15:18:27 up  1:44,  1 user,  load average: 0.28, 0.35, 0.25 47e7eb23be032fd0780f46f0fd748cb5d2c9ed2

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -35,7 +35,13 @@ static int cmd_c(char *args) {
 static int cmd_q(char *args) {
 	return -1;
 }
-
+static int cmd_si(char *args){
+	int steps =1;
+	if(args != NULL)
+		steps =atoi(args);
+	cpu_exec(steps);
+	return 0;
+}
 static int cmd_help(char *args);
 
 static struct {
@@ -46,7 +52,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+	{ "si", "fsjdkfls", cmd_si},
 	/* TODO: Add more commands */
 
 };
